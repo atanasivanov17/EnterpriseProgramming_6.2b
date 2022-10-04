08ea504c2e7316affe697f7f3ea8a2832e92826b
Atanas <a.ivanov9393@gmail.com>
2022-10-04 21:27:24 +0200

    Lesson 1 - first commit containing basic structure including projects with project references. It does not include nuget packages references yet

---
+++ EnterpriseProgramming/Domain/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int CategoryId { get; set; }
+        public int Stock { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
