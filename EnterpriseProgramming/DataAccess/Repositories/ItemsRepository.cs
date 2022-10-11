e7cb27ab3c007388b7171a18331702fea55b2fb1
Atanas <a.ivanov9393@gmail.com>
2022-10-11 16:01:25 +0200

    Lesson 3 - Added first migration and updated items repository

---
+++ EnterpriseProgramming/DataAccess/Repositories/ItemsRepository.cs
@@ -0,0 +1,43 @@
+﻿using DataAccess.Context;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Repositories
+{
+    class ItemsRepository
+    {
+        private ShoppingCartContext context;
+        public ItemsRepository(ShoppingCartContext _context)
+        {
+            this.context = _context;
+        }
+        public void AddItem(Item i)
+        {
+            context.Items.Add(i);
+            context.SaveChanges();
+        }
+
+        public void DeleteItem(Item i)
+        {
+            context.Items.Remove(i);
+        }
+
+        public Item GetItem(int id)
+        {
+            return null;
+        }
+
+        public IQueryable<Item> GetItems()
+        {
+            return null;
+        }
+
+        public void UpdateItem(Item originalItem, Item newItem)
+        {
+
+        }
+    }
+}
